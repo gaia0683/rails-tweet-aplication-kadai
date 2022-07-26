dfbbd5e414bc683ebd1b22162cfca24cec833f83
bomute kim <kimbomte1996@icloud.com>
2022-07-26 15:40:22 +0900

    新規投稿の画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+    def new
+     @post = Post.new
+    end    
+    
+    def create
+    Post.create(content: params[:post][:content])
+    redirect_to new_post_path
+    end    
 end
