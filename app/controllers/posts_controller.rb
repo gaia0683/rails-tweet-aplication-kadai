2b42d1a79059ef9cccd6bd816eba32a8e347ad13
bomute kim <kimbomte1996@icloud.com>
2022-07-26 16:11:42 +0900

    ポストの一覧画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+    end
+    
     def new
-     @post = Post.new
+       @post = Post.new
     end    
     
     def create
-    Post.create(content: params[:post][:content])
-    redirect_to new_post_path
+      Post.create(blog_params)
+      redirect_to new_post_path
     end    
+
+    def show
+      @post = Post.find(params[:id])
+    end    
+    
+    private
+
+    def blog_params
+      params.require(:post).permit(:content)
+    end
 end
