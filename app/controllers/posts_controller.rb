327549749fe959909d03fd3c84fa7867ca04b0a4
bomute kim <kimbomte1996@icloud.com>
2022-07-28 09:59:30 +0900

    編集、ポストの新規投稿Viewの共通処理化

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+    before_action :set_blog, only: [:show, :edit, :update]
+    
     def index
       @posts = Post.all
     end
@@ -8,17 +10,35 @@ class PostsController < ApplicationController
     end    
     
     def create
-      Post.create(blog_params)
-      redirect_to new_post_path
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path, notice: "ポストを作成しました！"
+      else
+        render :new
+      end
     end    
 
+    def edit
+    end    
+    
+    def update
+      if @post.update(post_params)
+        redirect_to posts_path, notice: "ポストを編集しました！"
+      else
+        render :edit  
+    end
+  end    
+    
     def show
-      @post = Post.find(params[:id])
     end    
     
     private
 
-    def blog_params
+    def post_params
       params.require(:post).permit(:content)
     end
+
+    def set_blog
+      @psot = Post.find(params[:id])
+    end  
 end
